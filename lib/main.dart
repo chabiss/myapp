e3c1620afcbeeaec3cd2806c8d000754cebd7156
Charles Bissonnette <chabiss@microsoft.com>
2019-08-08 23:34:40 -0700

    Add Assets

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,37 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+@override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State {
+  @override
   build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Easy Text'),
+          title: Text('Easy money'),
+          backgroundColor: Color(0xFF00FFFF),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                onPressed: () {},
+                child: Text('Click Me'),
+              ),
+            ),
+            Card(
+                child: Column(children: <Widget>[
+              Image.asset('assets/rectangles.jpg'),
+              Text('My Image')
+            ]))
+          ],
         ),
       ),
     );
